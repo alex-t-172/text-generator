aae5f8a9289e63f933e51d7c650cec595bf8344b
Alex Thoma <35978202+alex-t-172@users.noreply.github.com>
2018-02-04 16:17:28 +0000

    Functionalized build_model.py

--- build_model.py
+++ build_model.py
@@ -14,74 +14,77 @@ from keras.layers import LSTM,TimeDistributed
 from time import sleep
 import sys
 
-artist = input('Type name of artist to build model for: ')
-text = open('{}_data.txt'.format(artist)).read().lower()
+def create_data(artist):
+    #artist = input('Type name of artist to build model for: ')
+    text = open('{}_data.txt'.format(artist)).read().lower()
 
-print('corpus length:', len(text))
+    print('corpus length:', len(text))
 
-chars = sorted(list(set(text)))
-print('total chars:', len(chars))
-char_indices = dict((c, i) for i, c in enumerate(chars))
-indices_char = dict((i, c) for i, c in enumerate(chars))
+    chars = sorted(list(set(text)))
+    print('total chars:', len(chars))
+    char_indices = dict((c, i) for i, c in enumerate(chars))
+    indices_char = dict((i, c) for i, c in enumerate(chars))
 
-# split the corpus into sequences of length=maxlen
-#input is a sequence of 40 chars and target is also a sequence of 40 chars shifted by one position
-#for eg: if you maxlen=3 and the text corpus is abcdefghi, your input ---> target pairs will be
-# [a,b,c] --> [b,c,d], [b,c,d]--->[c,d,e]....and so on
-maxlen = 40
-step = 1
-sentences = []
-next_chars = []
-for i in range(0, len(text) - maxlen+1, step):
-    sentences.append(text[i: i + maxlen]) #input seq is from i to i  + maxlen
-    next_chars.append(text[i+1:i +1+ maxlen]) # output seq is from i+1 to i+1+maxlen
+    # split the corpus into sequences of length=maxlen
+    #input is a sequence of 40 chars and target is also a sequence of 40 chars shifted by one position
+    #for eg: if you maxlen=3 and the text corpus is abcdefghi, your input ---> target pairs will be
+    # [a,b,c] --> [b,c,d], [b,c,d]--->[c,d,e]....and so on
+    maxlen = 40
+    step = 1
+    sentences = []
+    next_chars = []
+    for i in range(0, len(text) - maxlen+1, step):
+        sentences.append(text[i: i + maxlen]) #input seq is from i to i  + maxlen
+        next_chars.append(text[i+1:i +1+ maxlen]) # output seq is from i+1 to i+1+maxlen
 
-print('nb sequences:', len(sentences))
+    print('nb sequences:', len(sentences))
 
-print('Vectorization...')
-X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
-y = np.zeros((len(sentences),maxlen, len(chars)), dtype=np.bool) # y is also a sequence , or  a seq of 1 hot vectors
-for i, sentence in enumerate(sentences):
-    for t, char in enumerate(sentence):
-        X[i, t, char_indices[char]] = 1
+    print('Vectorization...')
+    X = np.zeros((len(sentences), maxlen, len(chars)), dtype=np.bool)
+    y = np.zeros((len(sentences),maxlen, len(chars)), dtype=np.bool) # y is also a sequence , or  a seq of 1 hot vectors
+    for i, sentence in enumerate(sentences):
+        for t, char in enumerate(sentence):
+            X[i, t, char_indices[char]] = 1
 
-for i, sentence in enumerate(next_chars):
-    for t, char in enumerate(sentence):
-        y[i, t, char_indices[char]] = 1
+    for i, sentence in enumerate(next_chars):
+        for t, char in enumerate(sentence):
+            y[i, t, char_indices[char]] = 1
+            
+    return X, y, char_indices, indices_char, chars
     
 
-# build the model: 2 stacked LSTM
-print('Build model...')
-model = Sequential()
-model.add(LSTM(512, input_dim=len(chars),return_sequences=True))
-model.add(LSTM(512, return_sequences=True)) #- original
-model.add(Dropout(0.2))
-model.add(TimeDistributed(Dense(len(chars))))
-model.add(Activation('softmax'))
+def build_LSTM_model(X, y, chars):
+    # build the model: 2 stacked LSTM
+    print('Building model...')
+    model = Sequential()
+    model.add(LSTM(512, input_dim=len(chars),return_sequences=True))
+    model.add(LSTM(512, return_sequences=True)) #- original
+    model.add(Dropout(0.2))
+    model.add(TimeDistributed(Dense(len(chars))))
+    model.add(Activation('softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-print ('model is made')
-
-# train the model
-
-
-print (model.summary())
+    print ('model is made')
+    print (model.summary())
+    
+    return model
 
 
-for iteration in range(1, 6):
-    print()
-    print('-' * 50)
-    print('Iteration', iteration)
+def train_LSTM_model(model, X, y, artist):
+    for iteration in range(1, 6):
+        print()
+        print('-' * 50)
+        print('Iteration', iteration)
         
-    history=model.fit(X, y, batch_size=32, nb_epoch=1,verbose=0)    #small batch size for faster training? try 16 or 32
-    sleep(0.1) # https://github.com/fchollet/keras/issues/2110
+        history=model.fit(X, y, batch_size=32, nb_epoch=1,verbose=0)    #small batch size for faster training? try 16 or 32
+        sleep(0.1) # https://github.com/fchollet/keras/issues/2110
 
 
-    model.save('{}_deeperLSTM_model{}.h5'.format(artist, iteration))
+        model.save('{}_deeperLSTM_model{}.h5'.format(artist, iteration))
 
-    sys.stdout.flush()
-    print ('loss is')
-    print (history.history['loss'][0])
-    print (history.history)
-    print()
+        sys.stdout.flush()
+        print ('training history:')
+        print (history.history['loss'][0])
+        print (history.history)
+        print()

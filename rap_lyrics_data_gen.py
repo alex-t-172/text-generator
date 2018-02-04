6e370375d06a2865b204a459a6f9acd17060f10d
Alex Thoma <35978202+alex-t-172@users.noreply.github.com>
2018-02-04 16:04:56 +0000

    Functionalized rap_lyrics_data_gen.py

--- rap_lyrics_data_gen.py
+++ rap_lyrics_data_gen.py
@@ -3,52 +3,54 @@
 import requests
 import bs4
 
-artist = input('Type name of artist to download text file for: ')
+def generate_ohhla_text():
+    artist = input('Type name of artist to download text file for: ')
 
-main_page = requests.get('http://www.ohhla.com/YFA_{}.html'.format(artist))
-main_page.raise_for_status()
-main_page = bs4.BeautifulSoup(main_page.text)
+    main_page = requests.get('http://www.ohhla.com/YFA_{}.html'.format(artist))
+    main_page.raise_for_status()
+    main_page = bs4.BeautifulSoup(main_page.text)
 
 
-elems = main_page.select('a')
-filtered = []
-refiltered = []
+    elems = main_page.select('a')
+    filtered = []
+    refiltered = []
 
-for i in elems:
-    try:
-        dic = i.attrs
-        end_string = dic['href']
-        if end_string[-4:] == '.txt':
-            web_string = 'http://www.ohhla.com/' + end_string
-            filtered.append(web_string)
-    except:
-        continue
+    for i in elems:
+        try:
+            dic = i.attrs
+            end_string = dic['href']
+            if end_string[-4:] == '.txt':
+                web_string = 'http://www.ohhla.com/' + end_string
+                filtered.append(web_string)
+        except:
+            continue
     
-for i in filtered:
-    if artist in i:
-        refiltered.append(i)
+    for i in filtered:
+        if artist in i:
+            refiltered.append(i)
 
-data_file = open('{}_data.txt'.format(artist), 'w')
+    data_file = open('{}_data.txt'.format(artist), 'w')
 
 
-for song in refiltered:
-    try:
-        res = requests.get(song)
-        res.raise_for_status()
-        text = res.text
-        filter_text = bs4.BeautifulSoup(text)
-        elem_txt = filter_text.select('pre')
-        elem_txt = elem_txt[0].getText()
+    for song in refiltered:
+        try:
+            res = requests.get(song)
+            res.raise_for_status()
+            text = res.text
+            filter_text = bs4.BeautifulSoup(text)
+            elem_txt = filter_text.select('pre')
+            elem_txt = elem_txt[0].getText()
         
-        elem_txt = elem_txt.rpartition('Typed by: ')[-1]
-        #elem_txt = elem_txt.rpartition('\\n')[-1]
-        elem_txt = elem_txt.splitlines()[2:]
-        elem_txt = '\n'.join(elem_txt)
+            elem_txt = elem_txt.rpartition('Typed by: ')[-1]
+            #elem_txt = elem_txt.rpartition('\\n')[-1]
+            elem_txt = elem_txt.splitlines()[2:]
+            elem_txt = '\n'.join(elem_txt)
 
-        data_file.write(elem_txt)
-        print(str(song) + ' written')
-    except:
-        print(str(song) + ' not downloaded')
+            data_file.write(elem_txt)
+            print(str(song) + ' written')
+        except:
+            print(str(song) + ' not downloaded')
+    data_file.close()
     
-
-data_file.close()
+    print()
+    print('Text data file for {} generated'.format(artist))

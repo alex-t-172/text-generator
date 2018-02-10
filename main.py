22d5a9eb82ea9edd96a6b06561ee541bf69dbcf4
Alex Thoma <35978202+alex-t-172@users.noreply.github.com>
2018-02-10 21:42:49 +0000

    Added ability to generate text with regular specified commands, generalized input structure

--- main.py
+++ main.py
@@ -4,29 +4,32 @@ import rap_lyrics_data_gen
 import build_model
 import text_rnn_generate
 
-def full_run(download_flag):
-    '''Download song lyrics for, vectorize, build and train model for specified artist.'''
-    
-    artist = input('Type name of artist to work with: ')
-    
-    if download_flag == 'Y':
-        rap_lyrics_data_gen.generate_ohhla_text(artist)
-    
-    text, char_indices, indices_char, chars = build_model.create_indices(artist)
-    X, y, = build_model.create_data_arrays(text, artist, char_indices, indices_char, chars)
+def create_model(data_name):
+    '''Vectorize, build and train model for specified dataset.'''
+
+    text, char_indices, indices_char, chars = build_model.create_indices(data_name)
+    X, y, = build_model.create_data_arrays(text, data_name, char_indices, indices_char, chars)
     model = build_model.build_LSTM_model(chars)
-    build_model.train_LSTM_model(model, X, y, artist)
-    text_rnn_generate.generate_text(artist, chars, char_indices, indices_char)
+    build_model.train_LSTM_model(model, X, y, data_name)
 
 
-def text_gen_run():
-    '''Generate output text for specified artist - for pre trained model.'''
+def text_gen_run(data_name):
+    '''Generate bulk output text for specified dataset - for pre trained model.'''
     
-    artist = input('Type name of artist to work with: ')
-    text, char_indices, indices_char, chars = build_model.create_indices(artist)
-    text_rnn_generate.generate_text(artist, chars, char_indices, indices_char)
+    text, char_indices, indices_char, chars = build_model.create_indices(data_name)
+    text_rnn_generate.generate_text(data_name, chars, char_indices, indices_char)
     
 
+def text_iter_gen_run(data_name):
+    '''Generate bulk output text iteratively for specified dataset - for pre trained model.'''
+    
+    text, char_indices, indices_char, chars = build_model.create_indices(data_name)
+    text_rnn_generate.generate_iterative_text(data_name, chars, char_indices, indices_char)
+    
+    
 if __name__ == '__main__':
-    full_run('Y')
-    text_gen_run()
+    data_name = 'bb'
+    ##rap_lyrics_data_gen.generate_ohhla_text(artist)
+    create_model(data_name)
+    text_gen_run(data_name)
+    #text_iter_gen_run(data_name)
--- text_rnn_generate.py
+++ text_rnn_generate.py
@@ -5,34 +5,76 @@ import numpy as np
 from keras.models import load_model
 
 
-def generate_text(artist, chars, char_indices, indices_char):
-    '''Load in trained model of specified iteration, generate some output text based off a seed string.'''
+def generate_bulk_text(artist, chars, char_indices, indices_char):
+    '''Load in trained model of specified iteration, generate some output text 
+    based off a seed string and save to file.'''
     
-    iteration = input('Type iteration of model to load: ')
+    while True:
+        iteration = input('Type iteration of model to load ("exit to exit"): ')
+        
+        if iteration == 'exit':
+            sys.exit()
 
+        model_name = '{}_LSTM_model{}.h5'.format(artist, iteration)
+        model = load_model(model_name)
+
+        writing_range = input('Please specify no of characters to generate:')
+
+        seed_string=input('Please specify a seed string:')
+
+        outfile = open('{}_LSTM_model{}_{}.txt'.format(artist, iteration, seed_string), 'w')
+        outfile.write(seed_string)
+
+        for i in range(int(writing_range)):
+            x=np.zeros((1, len(seed_string), len(chars)))
+            for t, char in enumerate(seed_string):
+                x[0, t, char_indices[char]] = 1.
+            preds = model.predict(x, verbose=0)[0]
+            next_index=np.argmax(preds[len(seed_string)-1])
+    
+    
+            next_char = indices_char[next_index]
+            seed_string = seed_string + next_char
+
+            outfile.write(next_char)
+
+        outfile.close()    
+
+
+def generate_iterative_text(artist, chars, char_indices, indices_char):
+    '''Load in trained model of specified iteration, generate output text 
+    based off a seed strings specified every X characters, and save to file.'''
+    
+    iteration = input('Type iteration of model to load: ')    
+    
     model_name = '{}_LSTM_model{}.h5'.format(artist, iteration)
     model = load_model(model_name)
+    outfile = open('{}_LSTM_model{}.txt'.format(artist, iteration), 'w')
+    
+    while True:
+        
+        seed_string=input('Please specify a seed string (or "finish" to finish writing):')
+        if seed_string == 'finish':
+            break
 
-    writing_range = input('Please specify no of characters to generate:')
+        writing_range = input('Please specify no of characters to generate:')
 
-    seed_string=input('Please specify a seed string:')
-    print ("seed string -->", seed_string)
-    print ('The generated text is')
-    sys.stdout.write(seed_string),
+        print (seed_string)
+        outfile.write(seed_string)
 
-
-
-    for i in range(int(writing_range)):
-        x=np.zeros((1, len(seed_string), len(chars)))
-        for t, char in enumerate(seed_string):
-            x[0, t, char_indices[char]] = 1.
-        preds = model.predict(x, verbose=0)[0]
-        next_index=np.argmax(preds[len(seed_string)-1])
+        for i in range(int(writing_range)):
+            x=np.zeros((1, len(seed_string), len(chars)))
+            for t, char in enumerate(seed_string):
+                x[0, t, char_indices[char]] = 1.
+            preds = model.predict(x, verbose=0)[0]
+            next_index=np.argmax(preds[len(seed_string)-1])
     
     
-        next_char = indices_char[next_index]
-        seed_string = seed_string + next_char
-
-        sys.stdout.write(next_char)
-
-    sys.stdout.flush()    
+            next_char = indices_char[next_index]
+            seed_string = seed_string + next_char
+            
+            sys.stdout.write(next_char)
+            outfile.write(next_char)
+            
+        sys.stdout.flush() 
+    outfile.close()   
